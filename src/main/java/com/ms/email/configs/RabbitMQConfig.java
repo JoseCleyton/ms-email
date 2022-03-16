408c162972d3353e3c3cbdb4784e7682e7304dba
jose.de.araujo.souza <jose.de.araujo.souza@avanade.com>
2022-03-16 12:18:40 -0300

    Configuration RabbitQM

---
+++ src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.ms.email.configs;
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String queue = "ms.email";
+    public static final String NAME_EXCHANGE = "ExchangeEmail";
+    public static final String ROUTING_KEY = "email";
+
+    @Bean
+    DirectExchange emailExchange() {
+        return new DirectExchange(NAME_EXCHANGE);
+    }
+
+    @Bean
+    Queue queue() {
+        return QueueBuilder.durable(queue).build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(
+                queue()
+        ).to(emailExchange()).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
---
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,31 @@
+package com.ms.email.consumers;
+
+import com.ms.email.configs.RabbitMQConfig;
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(RabbitMQConfig.queue),
+            exchange = @Exchange(name = RabbitMQConfig.NAME_EXCHANGE),
+            key = RabbitMQConfig.ROUTING_KEY))
+    public void listen(EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+}
